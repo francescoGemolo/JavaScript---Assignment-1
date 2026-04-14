7649dbc844c7a6359b55d214fa5b0403c161591a
Francesco <francescolavoro0209@gmail.com>
2026-04-14 18:04:21 +0200

    Add 'Exit' Mode

--- game.js
+++ game.js
@@ -1,33 +1,39 @@
-// Generate random number (1-100)
+// Generate a random number (1-100)
 function generateRandomNumber() {
     return Math.floor(Math.random() * 100) + 1;
 }
 
-// Get valid user input
+// Get valid input or escape
 function getPlayerGuess(message) {
     let input = prompt(message);
 
+    if (input === null) {
+        alert("You escaped...for now!");
+        return null;
+    }
+
     while (
-        input === null ||
         input.trim() === "" ||
         isNaN(Number(input)) ||
         Number(input) < 1 ||
         Number(input) > 100 ||
         !Number.isInteger(Number(input))
     ) {
+        input = prompt("Foolish human!\nEnter a number (1-100)");
+
+        // Check escape again
         if (input === null) {
-            input = prompt("No escape Human!\nTry again.");
-        } else {
-            input = prompt("Invalid number (1-100).\nTry again.");
+            alert("You escaped...for now!");
+            return null;
         }
     }
 
     return Number(input);
 }
 
-// Compare guess with correct number
+// Check guess result
 function checkGuess(guess, correctNumber) {
-    if (guess < correctNumber) return "Too low :(";
+    if (guess < correctNumber) return "Too low!";
     if (guess > correctNumber) return "Too high!";
     return "Correct!";
 }
@@ -38,39 +44,51 @@ function game() {
     let attempts = 0;
     let maxAttempts = 10;
 
-    let intro = `Hello Human...\n
-                Guess the number (1-100).\n
-                You have 10 attempts.`;
+    let intro = `Hello human...\n
+I am the evil AI.\n
+Guess the number (1-100)\n
+You have 10 attempts\n
+Try to survive`;
 
     while (attempts < maxAttempts) {
         let guess;
 
         if (attempts === 0) {
             guess = getPlayerGuess(intro);
-        } else if (attempts === maxAttempts - 1) {
-            guess = getPlayerGuess("Last chance Human!");
-        } else {
+        }
+
+        else if (attempts === maxAttempts - 1) {
+            guess = getPlayerGuess("Last chance human...\nDo not fail!");
+        }
+
+        else {
             guess = getPlayerGuess(`Wrong!\nAttempts left: ${maxAttempts - attempts}`);
         }
+
+        // Exit if player escape
+        if (guess === null) return;
 
         attempts++;
 
         let result = checkGuess(guess, correctNumber);
 
-        if (result === "correct") {
-            // calculate score
+        // FIX QUI
+        if (result === "Correct!") {
             let score = (maxAttempts - attempts + 1) * 10;
 
-            alert(`YOU WIN!\nAttempts: ${attempts}\nScore: ${score}`);
+            alert(`Impossible...\nYou win!\nAttempts: ${attempts}\nScore: ${score}`);
             return;
-        } else if (result === "too low") {
-            alert("Too low!");
+        }
+
+        // FIX QUI
+        if (result === "Too low!") {
+            alert("Too low...");
         } else {
-            alert("Too high!");
+            alert("Too high, you dare?");
         }
     }
 
-    alert(`GAME OVER!\nNumber was: ${correctNumber}`);
+    alert(`Game over.\nThe world is mine.\nNumber was: ${correctNumber}`);
 }
 
 // Start game
